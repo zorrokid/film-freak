d21dd8caefd5e13318f3be92e7fa3255b95ed5f8
Mikko Keinänen <mikko.keinanen@gmail.com>
2022-10-21 21:53:40 +0300

    set painting style

--- lib/painters/image_text_block_painter.dart
+++ lib/painters/image_text_block_painter.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 import 'dart:ui' as ui;
+
+import 'package:path/path.dart';
 
 class ImageTextBlockPainter extends CustomPainter {
   ImageTextBlockPainter({required this.image, required this.textBlocks});
@@ -9,9 +12,13 @@ class ImageTextBlockPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
+    var paint = Paint();
+    paint.style = PaintingStyle.stroke;
+    paint.strokeWidth = 2;
+    paint.color = Colors.blue;
     canvas.drawImage(image, Offset.zero, Paint());
     for (var block in textBlocks) {
-      canvas.drawRect(block.boundingBox, Paint());
+      canvas.drawRect(block.boundingBox, paint);
     }
   }
 
