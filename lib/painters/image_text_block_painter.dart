ccc579b215d9b2bd5a95d4a25054f9130c7c5630
Mikko Keinänen <mikko.keinanen@gmail.com>
2022-10-23 01:59:30 +0300

    paint by word & interactiveviewer

--- lib/painters/image_text_block_painter.dart
+++ lib/painters/image_text_block_painter.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 import 'dart:ui' as ui;
 
-import 'package:path/path.dart';
+enum TextBlockPainterMode { paintByBlock, paintByWord }
 
 class ImageTextBlockPainter extends CustomPainter {
-  ImageTextBlockPainter({required this.image, required this.textBlocks});
+  ImageTextBlockPainter(
+      {required this.image, required this.textBlocks, required this.mode});
   final ui.Image image;
   final List<TextBlock> textBlocks;
+  TextBlockPainterMode mode;
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -17,12 +18,30 @@ class ImageTextBlockPainter extends CustomPainter {
     paint.strokeWidth = 2;
     paint.color = Colors.blue;
     canvas.drawImage(image, Offset.zero, Paint());
+    mode == TextBlockPainterMode.paintByWord
+        ? _paintByWord(canvas, paint)
+        : _paintByBlock(canvas, paint);
+  }
+
+  @override
+  bool shouldRepaint(ImageTextBlockPainter oldDelegate) =>
+      image != oldDelegate.image ||
+      textBlocks != oldDelegate.textBlocks ||
+      mode != oldDelegate.mode;
+
+  void _paintByBlock(Canvas canvas, Paint paint) {
     for (var block in textBlocks) {
       canvas.drawRect(block.boundingBox, paint);
     }
   }
 
-  @override
-  bool shouldRepaint(ImageTextBlockPainter oldDelegate) =>
-      image != oldDelegate.image || textBlocks != oldDelegate.textBlocks;
+  void _paintByWord(Canvas canvas, Paint paint) {
+    for (var block in textBlocks) {
+      for (var line in block.lines) {
+        for (var element in line.elements) {
+          canvas.drawRect(element.boundingBox, paint);
+        }
+      }
+    }
+  }
 }
--- lib/screens/image_text_selector.dart
+++ lib/screens/image_text_selector.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-import 'dart:math';
 import 'dart:ui' as ui;
 import 'package:film_freak/painters/image_text_block_painter.dart';
 import 'package:flutter/material.dart';
@@ -40,15 +39,19 @@ class _ImageTextSelectorState extends State<ImageTextSelector> {
   String _selectedText = '';
   bool _isReady = false;
   bool _isProcessing = false;
+  bool _showTextByWords = false;
   ui.Image? _image = null;
   final TextRecognizer _textRecognizer = TextRecognizer();
+  final TransformationController _transformationController =
+      TransformationController();
 
   void onReadyPressed(BuildContext context) {
     Navigator.pop(context, _selectedText);
   }
 
   void _onTapDown(TapDownDetails details, BuildContext context) {
-    if (_image == null) return;
+    if (_image == null || _textBlocks.isEmpty) return;
+
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
 
@@ -64,7 +67,6 @@ class _ImageTextSelectorState extends State<ImageTextSelector> {
     final posY = pos.dy * scaleY;
     print('x: ${pos.dx} y: ${pos.dy}');
     print('scaled x: $posX y: $posY');
-    if (_textBlocks.isEmpty) return;
 
     for (var i = 0; i < _textBlocks.length; i++) {
       final boundingBox = _textBlocks[i].boundingBox;
@@ -72,6 +74,20 @@ class _ImageTextSelectorState extends State<ImageTextSelector> {
           posX < boundingBox.right &&
           posY > boundingBox.top &&
           posY < boundingBox.bottom) {
+        // TextBlock
+        // - BoundingBox
+        // - List<TextLine> lines
+        // - String text
+        // - List<Point<int>> cornerPoints
+        // TextLine
+        // - Rect boundingBox
+        // - List<TextElement> elements
+        // - String text
+        // - List<Point<int>> cornerPoints
+        // TextElement
+        // - Rect boundingBox
+        // - String text
+        // - List<Point<int>> cornerPoints
         print('Selected box: $i');
         print('Selected text: ${_textBlocks[i].text}');
       }
@@ -91,20 +107,34 @@ class _ImageTextSelectorState extends State<ImageTextSelector> {
     }
     return Scaffold(
       appBar: AppBar(title: const Text('Pick text')),
-      body: Column(children: [
-        !_isProcessing && _image != null
-            ? FittedBox(
-                child: GestureDetector(
-                    onTapDown: (details) => {_onTapDown(details, context)},
-                    child: SizedBox(
-                        width: _image!.width.toDouble(),
-                        height: _image!.height.toDouble(),
-                        child: CustomPaint(
-                          painter: ImageTextBlockPainter(
-                              image: _image!, textBlocks: _textBlocks),
-                        ))))
-            : const CircularProgressIndicator()
-      ]),
+      body: !_isProcessing && _image != null
+          ? FittedBox(
+              child: Column(children: [
+              InteractiveViewer(
+                  transformationController: _transformationController,
+                  child: GestureDetector(
+                      onTapDown: (details) => {_onTapDown(details, context)},
+                      child: SizedBox(
+                          width: _image!.width.toDouble(),
+                          height: _image!.height.toDouble(),
+                          child: CustomPaint(
+                            painter: ImageTextBlockPainter(
+                                image: _image!,
+                                textBlocks: _textBlocks,
+                                mode: TextBlockPainterMode.paintByWord),
+                          )))),
+              Row(children: [
+                const Text('Show text by words:'),
+                Switch(
+                    value: _showTextByWords,
+                    onChanged: (bool value) {
+                      setState(() {
+                        _showTextByWords = value;
+                      });
+                    })
+              ])
+            ]))
+          : const CircularProgressIndicator(),
       floatingActionButton: FloatingActionButton(
         onPressed: () => onReadyPressed(context),
         backgroundColor: Colors.green,
