2c9732dc9f25ca07211ab80028f21c9053f92244
Mikko Keinänen <mikko.keinanen@gmail.com>
2022-11-25 00:30:35 +0200

    fetch tmdb config & show posters

--- lib/features/tmdb_search/tmdb_movie_search_screen.dart
+++ lib/features/tmdb_search/tmdb_movie_search_screen.dart
@@ -1,7 +1,9 @@
+import 'package:film_freak/features/tmdb_search/tmdb_configuration.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
 
 import './tmdb_movie_result.dart';
-import './tmdb_search.dart';
+import 'tmdb_search_service.dart';
 import '../../widgets/error_display_widget.dart';
 import '../../widgets/spinner.dart';
 
@@ -15,12 +17,16 @@ class TmdbMovieSearchScreen extends StatefulWidget {
 }
 
 class _TmdbMovieSearchScreenState extends State<TmdbMovieSearchScreen> {
+  final _searchService = TmdbSearchService();
+
   late Future<List<TmdbMovieResult>> _futureSearchResult;
+  late Future<TmdbConfiguration?> _futureConfiguration;
 
   @override
   void initState() {
     super.initState();
-    _futureSearchResult = searchMovies(widget.searchText);
+    _futureSearchResult = _searchService.getMovies(widget.searchText);
+    _futureConfiguration = _searchService.getConfiguration();
   }
 
   @override
@@ -30,8 +36,8 @@ class _TmdbMovieSearchScreenState extends State<TmdbMovieSearchScreen> {
         title: const Text('Results'),
       ),
       body: FutureBuilder(
-        future: _futureSearchResult,
-        builder: (context, snapshot) {
+        future: Future.wait([_futureConfiguration, _futureSearchResult]),
+        builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
           if (snapshot.hasError) {
             return ErrorDisplayWidget(
               snapshot.error.toString(),
@@ -40,12 +46,18 @@ class _TmdbMovieSearchScreenState extends State<TmdbMovieSearchScreen> {
           if (!snapshot.hasData) {
             return const Spinner();
           }
-          final results = snapshot.data!;
+          final TmdbConfiguration config = snapshot.data![0];
+          final List<TmdbMovieResult> results = snapshot.data![1];
           return ListView.builder(
             itemCount: results.length,
             itemBuilder: (context, index) {
               return ListTile(
                 title: Text(results[index].title),
+                subtitle: Text(results[index].originalTitle),
+                leading: results[index].posterPath.isNotEmpty
+                    ? Image.network(
+                        '${config.secureBaseUrl}${config.posterSizes[0]}/${results[index].posterPath}')
+                    : const Icon(Icons.image),
               );
             },
           );
