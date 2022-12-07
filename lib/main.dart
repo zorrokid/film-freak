70e5709518e245434cf5c7d0b3f2810cc13a9945
Mikko Keinänen <mikko.keinanen@gmail.com>
2022-12-07 19:31:57 +0200

    import fix

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:provider/provider.dart';
 import 'package:camera/camera.dart';
-import 'package:film_freak/screens/scan_view.dart';
+import 'package:film_freak/features/scan_barcode/scan_view.dart';
 import 'package:film_freak/init/logging.dart';
 import 'init/remote_config.dart';
 import 'init/state.dart';
