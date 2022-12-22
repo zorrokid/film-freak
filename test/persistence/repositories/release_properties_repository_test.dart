9767d90d307a00e4dc218c217d12f2b29fd71d7b
Mikko Keinänen <mikko.keinanen@gmail.com>
2022-12-23 01:22:46 +0200

    Unit testsi for releasePropertiesRepository

--- test/persistence/repositories/release_properties_repository_test.dart
+++ test/persistence/repositories/release_properties_repository_test.dart
@@ -22,17 +22,57 @@ Future main() async {
     const releaseId = 11;
     final releaseProperty = ReleaseProperty(
         propertyType: ReleasePropertyType.slipCover, releaseId: releaseId);
+    final expectedId =
+        await releasePropertiesRepository.insert(releaseProperty);
+    final insertedProperty = await releasePropertiesRepository.get(expectedId);
+    expect(insertedProperty.id, expectedId);
+    expect(insertedProperty.releaseId, releaseId);
+  });
+
+  test('Test getByReleaseId with release id value, it should return value.',
+      () async {
+    final releasePropertiesRepository =
+        ReleasePropertiesRepository(TestDatabaseProvider.instance);
+    const releaseId = 11;
+    final releaseProperty = ReleaseProperty(
+        propertyType: ReleasePropertyType.slipCover, releaseId: releaseId);
 
     final expectedId =
         await releasePropertiesRepository.insert(releaseProperty);
-
-    final insertedProperty = await releasePropertiesRepository.get(expectedId);
-
-    expect(insertedProperty.id, expectedId);
-    expect(insertedProperty.releaseId, releaseId);
-
     final result = await releasePropertiesRepository.getByReleaseId(releaseId);
     expect(result.length, 1);
+    expect(result.single.releaseId, releaseId);
     expect(result.single.id, expectedId);
+  });
+
+  test('Delete obsolete childs', () async {
+    final releasePropertiesRepository =
+        ReleasePropertiesRepository(TestDatabaseProvider.instance);
+    const releaseId = 11;
+    final releaseProperty1 = ReleaseProperty(
+        propertyType: ReleasePropertyType.slipCover, releaseId: releaseId);
+    final releaseProperty2 = ReleaseProperty(
+        propertyType: ReleasePropertyType.hologramCover, releaseId: releaseId);
+
+    final expectedId =
+        await releasePropertiesRepository.insert(releaseProperty1);
+    final _ = await releasePropertiesRepository.insert(releaseProperty2);
+
+    final properties =
+        await releasePropertiesRepository.getByReleaseId(releaseId);
+    expect(properties.length, 2);
+
+    final property1fromDb =
+        properties.where((element) => element.id == expectedId).single;
+
+    final newListOfProperties = <ReleaseProperty>[property1fromDb];
+
+    await releasePropertiesRepository.deleteObsoletedChilds(
+        releaseId, newListOfProperties);
+
+    final propertiesNew =
+        await releasePropertiesRepository.getByReleaseId(releaseId);
+    expect(propertiesNew.length, 1);
+    expect(propertiesNew.first.id, expectedId);
   });
 }
