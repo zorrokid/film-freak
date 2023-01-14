3b08274a7c170833678f7d6d497d037f5afcd1b4
Mikko Keinänen <mikko.keinanen@gmail.com>
2023-01-14 21:50:35 +0200

    remove condition from release media

--- lib/entities/release_media.dart
+++ lib/entities/release_media.dart
@@ -1,19 +1,15 @@
-import 'package:film_freak/enums/condition.dart';
-
 import '../enums/media_type.dart';
 import 'entity.dart';
 
 class ReleaseMedia extends ReleaseChildEntity<ReleaseMedia> {
   // when creating a new release id is set after entity is saved to db:
   MediaType mediaType;
-  Condition? condition;
   ReleaseMedia({
     int? releaseId,
     int? id,
     DateTime? createdTime,
     DateTime? modifiedTime,
     required this.mediaType,
-    this.condition,
   }) : super(
           id: id,
           releaseId: releaseId,
@@ -26,7 +22,6 @@ class ReleaseMedia extends ReleaseChildEntity<ReleaseMedia> {
         'id': id,
         'releaseId': releaseId,
         'mediaType': mediaType.index,
-        'condition': condition?.index ?? Condition.unknown,
         'createdTime': (createdTime ?? DateTime.now()).toIso8601String(),
         'modifiedTime': (modifiedTime ?? DateTime.now()).toIso8601String(),
       };
@@ -36,7 +31,6 @@ class ReleaseMedia extends ReleaseChildEntity<ReleaseMedia> {
       releaseId: map['releaseId'] as int,
       id: map['id'] as int,
       mediaType: MediaType.values[map['mediaType'] as int],
-      condition: Condition.values[map['condition'] as int],
       createdTime: DateTime.parse(map['createdTime'] as String),
       modifiedTime: DateTime.parse(map['modifiedTime'] as String),
     );
