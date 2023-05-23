c82cdf2d59f6ac6c4b00b003cfe1a17a0ed0faf7
Mikko Keinänen <mikko.keinanen@gmail.com>
2023-05-23 22:00:35 +0300

    fix state issue and improve spinner

--- lib/widgets/spinner.dart
+++ lib/widgets/spinner.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 
 class Spinner extends StatelessWidget {
-  const Spinner({super.key});
+  const Spinner({super.key, this.width = 60.0, this.height = 60.0});
+  final double width;
+  final double height;
   @override
   Widget build(BuildContext context) {
-    return const SizedBox(
-      width: 60,
-      height: 60,
-      child: CircularProgressIndicator(),
+    return Center(
+      child: SizedBox(
+        width: width,
+        height: height,
+        child: const CircularProgressIndicator(),
+      ),
     );
   }
 }
