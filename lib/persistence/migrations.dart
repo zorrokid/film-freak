77315a270635bdfd031af153c4febf25d48cc92b
Mikko Keinänen <mikko.keinanen@gmail.com>
2023-05-25 20:48:14 +0300

    add foreign key constraints

--- lib/persistence/migrations.dart
+++ lib/persistence/migrations.dart
@@ -6,7 +6,8 @@ final migrationScripts = <int, String>{
           barcode TEXT, 
           caseType INTEGER,
           createdTime DATETIME,
-          modifiedTime DATETIME)''',
+          modifiedTime DATETIME
+      )''',
   2: '''CREATE TABLE productions(
           id INTEGER PRIMARY KEY,
           productionType INTEGER,
@@ -16,63 +17,103 @@ final migrationScripts = <int, String>{
           overview STRING,
           releaseDate DATETIME,
           createdTime DATETIME,
-          modifiedTime DATETIME)''',
+          modifiedTime DATETIME
+      )''',
   3: '''CREATE TABLE releaseProductions(
           id INTEGER PRIMARY KEY,
           releaseId INTEGER,
           productionId INTEGER,
           createdTime DATETIME,
-          modifiedTime DATETIME)''',
+          modifiedTime DATETIME,
+          FOREIGN KEY (releaseId) 
+          REFERENCES releases(id)
+          ON DELETE CASCADE,
+          FOREIGN KEY (productionId) 
+          REFERENCES productions(id)
+          ON DELETE CASCADE
+      )''',
   4: '''CREATE TABLE collectionItems(
           id INTEGER PRIMARY KEY,
           releaseId INTEGER NOT NULL,
           condition INTEGER,
           status INTEGER,
           createdTime DATETIME,
-          modifiedTime DATETIME)''',
+          modifiedTime DATETIME,
+          FOREIGN KEY (releaseId)
+          REFERENCES releases(id)
+          ON DELETE CASCADE
+      )''',
   5: '''CREATE TABLE releasePictures(
           id INTEGER PRIMARY KEY, 
           releaseId INTEGER,
           filename TEXT,
           pictureType INTEGER,
           createdTime DATETIME,
-          modifiedTime DATETIME)''',
+          modifiedTime DATETIME,
+          FOREIGN KEY (releaseId)
+          REFERENCES releases(id)
+          ON DELETE CASCADE
+      )''',
   6: '''CREATE TABLE releaseProperties(
           id INTEGER PRIMARY KEY,
           releaseId INTEGER,
           propertyType INTEGER,
           createdTime DATETIME,
-          modifiedTime DATETIME)''',
+          modifiedTime DATETIME,
+          FOREIGN KEY (releaseId)
+          REFERENCES releases(id)
+          ON DELETE CASCADE
+      )''',
   7: '''CREATE TABLE collectionItemProperties(
           id INTEGER PRIMARY KEY,
           collectionItemId INTEGER,
           propertyType INTEGER,
           createdTime DATETIME,
-          modifiedTime DATETIME)''',
+          modifiedTime DATETIME,
+          FOREIGN KEY (collectionItemId)
+          REFERENCES collectionItems(id)
+          ON DELETE CASCADE
+      )''',
   8: '''CREATE TABLE releaseComments(
           id INTEGER PRIMARY KEY,
           releaseId INTEGER,
           comment TEXT,
           createdTime DATETIME,
-          modifiedTime DATETIME)''',
+          modifiedTime DATETIME,
+          FOREIGN KEY (releaseId)
+          REFERENCES releases(id)
+          ON DELETE CASCADE
+      )''',
   9: '''CREATE TABLE collectionItemComments(
           id INTEGER PRIMARY KEY,
           collectionItemId INTEGER,
           comment TEXT,
           createdTime DATETIME,
-          modifiedTime DATETIME)''',
+          modifiedTime DATETIME,
+          FOREIGN KEY (collectionItemId)
+          REFERENCES collectionItems(id)
+          ON DELETE CASCADE
+      )''',
   10: '''CREATE TABLE releaseMedias(
           id INTEGER PRIMARY KEY, 
           releaseId INTEGER,
           mediaType INTEGER,
           condition INTEGER,
           createdTime DATETIME,
-          modifiedTime DATETIME)''',
+          modifiedTime DATETIME,
+          FOREIGN KEY (releaseId)
+          REFERENCES releases(id)
+          ON DELETE CASCADE
+      )''',
   11: '''CREATE TABLE collectionItemMedias(
           id INTEGER PRIMARY KEY,
           collectionItemId INTEGER,
           releaseMediaId INTEGER,
-          condition INTEGER)''',
-  12: 'ALTER TABLE collectionItemMedias ADD COLUMN createdTime DATETIME',
-  13: 'ALTER TABLE collectionItemMedias ADD COLUMN modifiedTime DATETIME',
+          condition INTEGER,
+          createdTime DATETIME,
+          modifiedTime DATETIME,
+          FOREIGN KEY (collectionItemId)
+          REFERENCES collectionItems(id)
+          ON DELETE CASCADE
+      )''',
 };
