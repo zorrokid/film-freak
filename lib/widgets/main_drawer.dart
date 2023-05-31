92ff141bb492521ad361f43bcecafaaf1105bae6
Mikko Keinänen <mikko.keinanen@gmail.com>
2023-05-31 23:21:51 +0300

    AppStateOld no longer necessary

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -4,13 +4,11 @@ import 'package:film_freak/screens/data_sync/view/data_sync_view.dart';
 import 'package:film_freak/utils/dialog_utls.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:provider/provider.dart';
 import '../bloc/app_bloc.dart';
 import '../bloc/app_state.dart';
 import '../bloc/user_bloc.dart';
 import '../bloc/user_state.dart';
 import '../screens/log_in/view/log_in_page.dart';
-import '/persistence/app_state.dart';
 import '/screens/view_about/about_view.dart';
 
 class MainDrawer extends StatefulWidget {
@@ -32,47 +30,46 @@ void _navigateFromDrawer(BuildContext context, Widget widget) {
 class _MainDrawerState extends State<MainDrawer> {
   @override
   Widget build(BuildContext context) {
-    return Consumer<AppStateOld>(builder: (context, cart, child) {
-      return BlocConsumer<AppBloc, AppState>(
-        listener: (context, state) {
-          if (state.status == AppStatus.dbResetDone) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(
-                content: Text('Database reset done'),
-                duration: Duration(seconds: 1),
-              ),
-            );
-            _navigateFromDrawer(context, const ReleasesPage());
-          }
-        },
-        builder: (context, state) {
-          return BlocBuilder<UserBloc, UserState>(builder: (context, state) {
-            final userBloc = context.read<UserBloc>();
-            return Drawer(
-              child: ListView(
-                padding: EdgeInsets.zero,
-                children: [
-                  const DrawerHeader(
-                    decoration: BoxDecoration(color: Colors.blue),
-                    child: Text('Drawer header'),
-                  ),
-                  ListTile(
-                    title: const Text('Releases'),
-                    onTap: () =>
-                        _navigateFromDrawer(context, const ReleasesPage()),
-                  ),
-                  userBloc.state.status == UserStatus.loggedOut
-                      ? ListTile(
-                          title: const Text('Log in'),
-                          onTap: () =>
-                              _navigateFromDrawer(context, const LogInPage()),
-                        )
-                      : ListTile(
-                          title: const Text('Data'),
-                          onTap: () => _navigateFromDrawer(
-                              context, const DataSyncView()),
-                        ),
-                  /*ListTile(
+    return BlocConsumer<AppBloc, AppState>(
+      listener: (context, state) {
+        if (state.status == AppStatus.dbResetDone) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text('Database reset done'),
+              duration: Duration(seconds: 1),
+            ),
+          );
+          _navigateFromDrawer(context, const ReleasesPage());
+        }
+      },
+      builder: (context, state) {
+        return BlocBuilder<UserBloc, UserState>(builder: (context, state) {
+          final userBloc = context.read<UserBloc>();
+          return Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: [
+                const DrawerHeader(
+                  decoration: BoxDecoration(color: Colors.blue),
+                  child: Text('Drawer header'),
+                ),
+                ListTile(
+                  title: const Text('Releases'),
+                  onTap: () =>
+                      _navigateFromDrawer(context, const ReleasesPage()),
+                ),
+                userBloc.state.status == UserStatus.loggedOut
+                    ? ListTile(
+                        title: const Text('Log in'),
+                        onTap: () =>
+                            _navigateFromDrawer(context, const LogInPage()),
+                      )
+                    : ListTile(
+                        title: const Text('Data'),
+                        onTap: () =>
+                            _navigateFromDrawer(context, const DataSyncView()),
+                      ),
+                /*ListTile(
               title: const Text('Import'),
               onTap: () => _navigateFromDrawer(
                   context,
@@ -81,30 +78,28 @@ class _MainDrawerState extends State<MainDrawer> {
                         dbProvider: DatabaseProviderSqflite.instance),
                   )),
             ),*/
-                  ListTile(
-                    title: const Text('Delete'),
-                    onTap: () {
-                      confirm(
-                        context: context,
-                        title: 'Confirm DB delete',
-                        message:
-                            'Are you really sure you want to delete all the data in database?',
-                        onConfirm: () =>
-                            context.read<AppBloc>().add(const ResetDb()),
-                      );
-                    },
-                  ),
-                  ListTile(
-                    title: const Text('About'),
-                    onTap: () =>
-                        _navigateFromDrawer(context, const AboutView()),
-                  ),
-                ],
-              ),
-            );
-          });
-        },
-      );
-    });
+                ListTile(
+                  title: const Text('Delete'),
+                  onTap: () {
+                    confirm(
+                      context: context,
+                      title: 'Confirm DB delete',
+                      message:
+                          'Are you really sure you want to delete all the data in database?',
+                      onConfirm: () =>
+                          context.read<AppBloc>().add(const ResetDb()),
+                    );
+                  },
+                ),
+                ListTile(
+                  title: const Text('About'),
+                  onTap: () => _navigateFromDrawer(context, const AboutView()),
+                ),
+              ],
+            ),
+          );
+        });
+      },
+    );
   }
 }
